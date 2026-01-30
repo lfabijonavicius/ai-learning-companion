5f7c57d8d487773eb82d51e309fcdd3d2e41354b
Lukas F <lukasfab6@gmail.com>
2026-01-30 13:52:06 +0000

    Complete Question class

--- question.py
+++ question.py
@@ -1,18 +1,20 @@
 from typing import Dict, List, Optional
-import uuid #To generate unique ID's for each question
+import uuid # Generates unique ID for each question
 
+#Blueprint for creating question objects
 class Question:
     """Study question with performance tracking"""
-    
+
+#Creates a new Question object    
     def __init__(self, topic: str, text: str, question_type: str, correct_answer: str,
                  options: Optional[List[str]] = None, source: str = "manual", 
                  question_id: Optional[str] = None) -> None:
         
         
-        self.enabled = True
+        self.enabled = True #Initialize statistics 
         self.times_shown = 0
         self.times_correct = 0 
-        self.topic = topic
+        self.topic = topic #Store question data 
         self.text = text
         self.type = question_type
         self.correct_answer = correct_answer
@@ -36,12 +38,45 @@ class Question:
         
         if was_correct: 
             self.times_correct += 1  #Increment correct count 
-        
-        
+     
             
+    def to_dict(self) -> Dict:
+        """Converting questions to dictionary in JSON.
+        Returns dictionary containing all question data."""
         
-            
-            
-           
+        return {"id": self.id,
+                "topic": self.topic,
+                "text": self.text,
+                "type": self.type,
+                "correct_answer": self.correct_answer,
+                "options": self.options,
+                "source": self.source,
+                "enabled": self.enabled,
+                "times_shown": self.times_shown,
+                "times_correct": self.times_correct
+                }
     
+    #Creates a new instance     
+    @classmethod
+    def from_dict(cls, data: Dict) -> 'Question':
+        """Create a question using data from dictionary
+        Load questions from JSON file"""
     
+    #cls() is used to call the constructor with data form dictionary
+    #Converts dictionary to question object,
+        question = cls(
+           topic = data["topic"],
+           text = data["text"],
+           question_type = data["type"],
+           correct_answer = data["correct_answer"],
+           options = data.get("options", []),
+           source= data.get("source", "manual"),
+           question_id =data.get("id")
+       )
+       
+    #Statistics from saved data 
+        question.enabled = data.get("enabled", True)
+        question.times_shown = data.get("times_shown", 0)
+        question.times_correct = data.get("times_correct", 0)
+    
+        return question       
